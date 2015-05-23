9f86d09f911b5cfb18cd5cc1d4329c0a7230b0d9
gio <gio@isdev.net>
2015-05-22 20:36:17 -0400

    basic resolutions, with template and static data

---
+++ resolutions.js
@@ -0,0 +1,15 @@
+if (Meteor.isClient) {
+    Template.body.helpers({
+       resolutions: [
+        { title: "Hello Resolution #1" },
+        { title: "Bye Resolution #2" },
+        { title: "Mooo" }
+       ] 
+    });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
